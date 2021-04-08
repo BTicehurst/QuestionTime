600c466cda085440a2ed037f718e4bc6f226fc30
Brodie <brodieticehurst@hotmail.com>
2021-04-08 14:26:10 +0100

    First API endpoint + default SPA view
    Created first API endpoint, serializer & view to get current user's username (requires a current authenticated user).
    
    Added a default Vue SPA page to render according to a regex 'catch-all' url if entered url does not match a more specific one.

--- QuestionTime/urls.py
+++ QuestionTime/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 from django_registration.backends.one_step.views import RegistrationView
 
+from core.views import IndexTemplateView
 from users.forms import CustomUserForm
+
 
 # https://django-registration.readthedocs.io/en/3.1.1/activation-workflow.html
 
@@ -37,6 +39,9 @@ urlpatterns = [
     path("accounts/",
         include('django.contrib.auth.urls')),
 
+    path('api/',
+        include('users.api.urls')),
+
     path('api-auth/',
         include('rest_framework.urls')),
 
@@ -45,5 +50,7 @@ urlpatterns = [
 
     path('api/rest-auth/registration/',
         include('rest_auth.registration.urls')),
+
+    re_path(r"^.*$", IndexTemplateView.as_view(), name="entry-point")
 
 ]
