3059f48b1a18eecd167d623544bd8b157f5ec806
Brodie <brodieticehurst@hotmail.com>
2021-04-17 17:21:40 +0100

    Testing Vue
    -Created new page component and new prop
    -Configured a new router link for the component in the App.vue and router/index.js

---
+++ frontend/src/views/Example.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="example">
+    <h1>First single-file component</h1>
+    <h3>{{ message }}</h3>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+    name: "example",
+    data() {
+        return {
+            message: "Hello World!"
+        }
+    },
+
+    setup() {        
+    },
+})
+</script>
+
+
+<style scoped>
+    h1 {
+        color: red;
+    }
+</style>
