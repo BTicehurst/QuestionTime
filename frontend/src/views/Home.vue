49892fc952fdcac61a2be8a6f0af8ca2635e278c
Brodie <brodieticehurst@hotmail.com>
2021-04-22 18:33:45 +0100

    Created Question Component
    -index.js requires 'slug' param to access '/question'
    -Question component records slug as a prop
    -getQuestionData to run when instance is loaded -created()-
    -getQuestionData uses apiService to access an endpoint and save response data as 'question'
    -question returned when 'data()' is run
    -question content, author, and creation date rendered in template

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -4,9 +4,15 @@
       <div v-for="question in questions"
             :key="question.pk">
         <p class="mb-0">Posted By:
-          <span class="author-name">{{ question.author }}</span>
+          <span class="question-author">{{ question.author }}</span>
         </p>
-        <h2>{{ question.content }}</h2>
+        <h2>
+          <router-link
+            :to="{ name: 'question', params: { slug: question.slug }}"
+            class="question-link"
+          >{{ question.content }}
+          </router-link>
+        </h2>
         <p>Answers: {{ question.answers_count }}</p>
         <hr>
       </div>
@@ -15,7 +21,7 @@
 </template>
 
 <script>
-import { apiService } from "../common/api.service"
+import { apiService } from "../common/api.service.js"
 
 export default {
   name: "Home",
@@ -40,14 +46,25 @@ export default {
   },
   created() {
     this.getQuestions()
+    document.title = "QuestionTime";
   }
 };
 </script>
 
-<style>
-.author-name {
+<style scoped>
+.question-author {
   font-weight: bold;
   color: red;
+}
+
+.question-link {
+  font-weight: bold;
+  color: black;
+}
+
+.question-link:hover {
+  color: #62686D;
+  text-decoration: none;
 }
 
 </style>
---
+++ frontend/src/views/Question.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="single-question mt-2">
+        <div class="container">
+            <h1> {{ question.content }} </h1>
+            <p class="mb-0">Posted By:
+                <span class="author-name">{{ question.author }}</span>
+            </p>
+            <p>{{ question.created_at }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+// import { defineComponent } from '@vue/composition-api'
+import { apiService } from "../common/api.service.js"
+
+export default {
+    name: 'question',
+    props: {
+        slug: {
+            type: String,
+            required: true
+        }
+    },
+
+    data() {
+        return {
+            question: {}
+        }
+    },
+
+    methods: {
+        setPageTitle(title) {
+            document.title = title;
+        },
+        getQuestionData() {
+            let endpoint = `/api/questions/${this.slug}/`;
+            apiService(endpoint)
+                .then(data => {
+                   this.question = data; 
+                   this.setPageTitle(data.content)
+                })
+        }
+    },
+
+    created() {
+        this.getQuestionData()
+    }
+
+}
+</script>
+
+<style scoped>
+.author-name {
+  font-weight: bold;
+  color: red;
+}
+
+</style>
