85a9ead28e4de55f9fc642b8f057f9d0ca3576ab
Brodie <brodieticehurst@hotmail.com>
2021-06-02 14:06:12 +0100

    Added 'load more questions' button
    Added a 'Load More' button on home page to render questions beyond the default pagination number.
    Done by using the 'next' parameter passed in the api response data to grab the next 'page' of questions from django's api, and append it to the home component's 'questions' array

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -16,6 +16,15 @@
         <p>Answers: {{ question.answers_count }}</p>
         <hr>
       </div>
+      <div class="my-4">
+        <p v-show="loadingQuestions">...loading...</p>
+        <button
+          v-show="next"
+          @click="getQuestions"
+          class="btn btn-sm btn-outline-success"
+          >Load More
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -27,20 +36,33 @@ export default {
   name: "Home",
   data() {
     return {
-      questions: []
+      questions: [],
+      next: null,
+      loadingQuestions: false
     }
   },
   methods: {
     getQuestions() {
       let endpoint = "api/questions/";
+      if (this.next) {
+        endpoint = this.next;
+      }
+      this.loadingQuestions = true;
+
       apiService(endpoint)
         .then(data => {
 
-          // Use the below line 'objects.assign......' if not using pagination,
+          // Use the below line 'objects.assign...' ONLY if not using pagination,
           // otherwise the http response data will not have a 'results' attr
           //
           // Object.assign(this.questions, data);
           this.questions.push(...data.results)
+          this.loadingQuestions = false;
+          if (data.next) {
+            this.next = data.next;
+          } else {
+            this.next = null;
+          }
         })
     }
   },
--- frontend/src/views/QuestionEditor.vue
+++ frontend/src/views/QuestionEditor.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="container mt-2">
         <h1 class="mb-3">Ask a Question</h1>
+
+        <!-- override default form submit behaviour to call custom 'onSubmit'
+        method -->
         <form @submit.prevent="onSubmit">
             <textarea 
             v-model="question_body"
@@ -15,20 +18,32 @@
                 >Publish
             </button>
         </form>
-        <p v-if="error" class="muted mt-2">{{ error }}></p>
+        <p v-if="error" class="muted mt-2">{{ error }}</p>
     </div>
 </template>
 
 <script>
 import { apiService } from "../common/api.service.js"
+
+// below is everything that will get exported to the 'App.vue' file 
+
 export default {
     name: "QuestionEditor",
+    
+    // Data passes and initialises the models to be used within the component
+    // to App.vue, so they can be used within the methods: and template
+
     data() {
         return {
             question_body: null,
             error: null,
         }
     },
+
+    // declare methods to be usable within the component's template, or 
+    // override methods used within the base template/app.vue while this
+    // component is open
+
     methods: {
         onSubmit() {
             if (!this.question_body) {
@@ -38,6 +53,7 @@ export default {
             } else {
                 let endpoint = "api/questions/";
                 let method = "POST";
+
                 apiService(endpoint, method, {content: this.question_body})
                     .then(question_data => {
                         this.$router.push({
@@ -48,6 +64,7 @@ export default {
             }
         }
     },
+    // logic to be run when the component is loaded/created
     created() {
         document.title = "Editor - QuestionTime";
     },
